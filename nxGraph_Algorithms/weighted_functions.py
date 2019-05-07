121e72a881ea9c079f2ff36a3b5a4a4ccbe257cb
zlignore <49925715+zlignore@users.noreply.github.com>
2019-05-07 18:13:27 -0500

    Update weighted_functions.py

--- nxGraph_Algorithms/weighted_functions.py
+++ nxGraph_Algorithms/weighted_functions.py
@@ -4,33 +4,27 @@ from Functions.local_properties import *
 from Functions.bool_functions import *
 from Test_Graphs import *
 
-T = nx.read_weighted_edgelist('Test_Graphs/weightedG1.txt')
-G = nx.read_weighted_edgelist('Test_Graphs/weightedG1.txt')
-
-
 def cost(G, e):
     return G[e[0]][e[1]]['weight']
 
 def is_spanning(G, H):
     return set(V(G)) == set(V(H))
-def statrting_vertex(T, v):
-    return v
 
-def incident_edges(G, T, v):
+def incident_edges(G, T):
     edges = []
-    for i in V(T):
-        if i in neighbors(T, v):
-            edges.append(i)
+    for e in E(G):
+        if e[0] in V(T) or e[1] in V(T):
+            if e[0] not in V(T) or e[1] not in V(T):
+                edges.append(e)
     return edges
     
 def min_cost_edge(G, T):
-    
+    edges = incident_edges(G, T)
     min_edge = edges[0]
-    for e in incident_edges:
+    for e in edges:
         if cost(G, e) < cost(G, min_edge):
             min_edge = e
-        return min_edge
+    return min_edge
     
-
-def tree_cost(T):
-    return sum(min_cost_egde(G, T) for e in T[1])
+def tree_cost(G, T):
+    return sum([cost(G, e) for e in E(T)])
