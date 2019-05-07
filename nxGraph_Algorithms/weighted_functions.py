a3b078f5ce59be13d0196a58de77f6c28f91aca0
zlignore <49925715+zlignore@users.noreply.github.com>
2019-05-07 18:25:41 -0500

    Update weighted_functions.py

--- nxGraph_Algorithms/weighted_functions.py
+++ nxGraph_Algorithms/weighted_functions.py
@@ -5,12 +5,15 @@ from Functions.bool_functions import *
 from Test_Graphs import *
 
 def cost(G, e):
+    """Returns the cost of a given edge"""
     return G[e[0]][e[1]]['weight']
 
 def is_spanning(G, H):
+    """Confirms if the tree T is spanning to the graph G"""
     return set(V(G)) == set(V(H))
 
 def incident_edges(G, T):
+    """Returns a list of edges adjacent to the starting vertex"""
     edges = []
     for e in E(G):
         if e[0] in V(T) or e[1] in V(T):
@@ -19,6 +22,7 @@ def incident_edges(G, T):
     return edges
     
 def min_cost_edge(G, T):
+    """Returns the edge with the lowest cost"""
     edges = incident_edges(G, T)
     min_edge = edges[0]
     for e in edges:
@@ -27,4 +31,5 @@ def min_cost_edge(G, T):
     return min_edge
     
 def tree_cost(G, T):
+    """Returns the cost of all the edges in the tree T"""
     return sum([cost(G, e) for e in E(T)])
