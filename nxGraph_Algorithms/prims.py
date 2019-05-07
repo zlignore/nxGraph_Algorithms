9c5c1dba46a0a4b61cf1aa1bdd16864c13b6ffee
zlignore <49925715+zlignore@users.noreply.github.com>
2019-05-07 18:12:35 -0500

    Update prims.py

--- nxGraph_Algorithms/prims.py
+++ nxGraph_Algorithms/prims.py
@@ -1,12 +1,12 @@
 import networkx as nx
 from Functions.weighted_functions import *
 
-T = nx.read_weighted_edgelist('Test_Graphs/weightedG1.txt')
 G = nx.read_weighted_edgelist('Test_Graphs/weightedG1.txt')
 
 def prims(G, starting_vertex):
     T = nx.Graph()
     T.add_node(starting_vertex)
-    while is_spanning(G, T) == False:
-        T.add_node(min_cost_edge(G, T))
-    return T, tree_cost(T)
+    while is_spanning(G, T) != True:
+        e = min_cost_edge(G,T)
+        T.add_edge(e[0], e[1], weight = cost(G, e))
+    return T, tree_cost(G, T)
